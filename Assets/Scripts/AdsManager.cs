bd1fb34c8b1d7f798b55182057b444e834e66a93
Superjewew <norman.lie@mediavisi.com>
2016-06-27 18:18:23 +0700

    Init commit
    -Initial commit

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdsManager : MonoBehaviour {
+
+	public static AdsManager instance;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+	void ShowBannerAd() {
+
+	}
+
+	void HideBannerAd() {
+
+	}
+
+	void CacheInterstitialAd() {
+
+	}
+
+	void ShowInterstitialAd() {
+
+	}
+
+	void CacheRewardedVideo() {
+
+	}
+
+	void ShowRewardedVideo() {
+
+	}
+}
