5212207a88f8b315c08a9231062838f3155e65f2
Superjewew <norman.lie@mediavisi.com>
2016-07-04 04:59:24 +0700

    Finishing Reward video & console log
    - Add vungle library
    - Add console box & function
    - Add show rewarded video ad function
    - Create library androidinfo.jar
    - Create class AndroidInfo for handling device id and location
    - Add getdeviceid function

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -12,6 +12,7 @@ public class AdsManager : MonoBehaviour {
 
 	GoogleBannerAd googleAd;
 	ChartboostAd chartAd;
+	VungleAd vungleAd;
 
 	// Use this for initialization
 	void Awake () {
@@ -21,48 +22,62 @@ public class AdsManager : MonoBehaviour {
 			Destroy (gameObject);
 		}
 		DontDestroyOnLoad (gameObject);
+	}
 
+	public void Init() {
 		googleAd = new GoogleBannerAd ();
 		chartAd = new ChartboostAd ();
+		vungleAd = new VungleAd (consoleText);
+		consoleText.text += "Initialized = true\n";
 	}
 	
 	public void ShowBannerAd() {
 		googleAd.ShowBannerAd ();
+		consoleText.text += "Showing Banner Ad\n";
 	}
 
 	public void HideBannerAd() {
 		googleAd.HideBannerAd ();
+		consoleText.text += "Hiding Banner Ad\n";
 	}
 
 	public void CacheInterstitialAd() {
 		chartAd.CacheInterstitialAd ();
+		consoleText.text += "Caching Interstitial Ad\n";
 	}
 
 	public void ShowInterstitialAd() {
 		chartAd.ShowInterstitialAd ();
+		consoleText.text += "Showing Interstitial Ad\n";
 	}
 
-	void CacheRewardedVideo() {
+	public void CacheRewardedVideo() {
 
 	}
 
-	void ShowRewardedVideo() {
-
+	public void ShowRewardedVideo() {
+		if (Vungle.isAdvertAvailable ()) {
+			Vungle.playAd ();
+			consoleText.text += "Playing Rewarded Video Ad\n";
+		}
 	}
 
 	class GoogleBannerAd {
 
 		BannerView bannerView;
+		string adUnitId;
 
-		public void RequestBanner() {
+		public GoogleBannerAd() {
 			#if UNITY_ANDROID
-			string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+			adUnitId = "ca-app-pub-3940256099942544/6300978111";
 			#elif UNITY_IPHONE
 			string adUnitId = "INSERT_IOS_BANNER_AD_UNIT_ID_HERE";
 			#else
 			string adUnitId = "unexpected_platform";
 			#endif
+		}
 
+		public void RequestBanner() {
 			// Create a 320x50 banner at the top of the screen.
 			bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
 			// Create an empty ad request.
@@ -82,6 +97,8 @@ public class AdsManager : MonoBehaviour {
 	}
 
 	class ChartboostAd {
+
+
 		public void CacheInterstitialAd() {
 			Chartboost.cacheInterstitial(CBLocation.HomeScreen);
 		}
@@ -92,6 +109,19 @@ public class AdsManager : MonoBehaviour {
 			} else {
 				Debug.Log ("Ad not cached, not showing"); 
 			}
+		}
+	}
+
+	class VungleAd {
+
+		public VungleAd(Text consoleText) {
+
+
+			Vungle.init ("577947f3bc8623f86800006c", "", "");
+
+			Vungle.adPlayableEvent += (adPlayable) => {
+				consoleText.text += "Ad's playable state has been changed! Now: " + adPlayable + "\n";
+			};
 		}
 	}
 }
---
+++ Assets/Scripts/AndroidInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AndroidInfo : MonoBehaviour {
+
+	public Text consoleText;
+
+	AndroidDevice device = new AndroidDevice();
+
+	public void GetDeviceId() {
+		string id = device.GetDeviceId ();
+		consoleText.text += "Device Id: " + id + "\n";
+	}
+
+	class AndroidDevice {
+
+		public string GetDeviceId() {
+			AndroidJavaClass infoClass = new AndroidJavaClass ("com.meyourours.androidinfo.MainActivity");
+			string deviceId = infoClass.Call<string> ("GetDeviceId");
+			return deviceId;
+		}
+
+	}
+}
